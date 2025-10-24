4034554f8688a7b71175368983fe93f44967ee1f
Allison Bierschenk <allisonreynoldsc@gmail.com>
2025-10-23 19:38:35 -0500

    Initial commit: Angular Tableau Dashboard application

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, ElementRef, ViewChild, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import { TableauViz } from '@tableau/embedding-api';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
+
+  @ViewChild('viewingViz') viewingViz!: ElementRef;
+
+  private viewing!: TableauViz;
+  public vizUrl: string = 'https://public.tableau.com/views/ShopifyOverviewRAWNutritionConcept/ShopifyOverview?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link';
+
+  ngOnInit(): void {
+    // ViewChild is not available yet in ngOnInit
+  }
+
+  ngAfterViewInit(): void {
+    this.loadTableauViz();
+  }
+
+  ngOnDestroy(): void {
+    if (this.viewing) {
+      this.viewing.remove();
+    }
+  }
+
+  async loadTableauViz(): Promise<void> {
+    if (!this.viewingViz?.nativeElement) {
+      console.error('ViewChild viewingViz is not available');
+      return;
+    }
+
+    this.viewing = new TableauViz();
+    this.viewing.width = '100%';
+    this.viewing.height = '600px';
+    this.viewing.src = this.vizUrl;
+    this.viewingViz.nativeElement.appendChild(this.viewing);
+  }
+}
